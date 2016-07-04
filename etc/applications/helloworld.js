3b20f4a14b8dde878d8e0c7f7711a83c9cb6df40
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-07-04 19:59:55 +0200

    Updates with GKE firewall management and Dataporten provisioning. Not working properly though, will be fixed soon

--- etc/applications/helloworld.js
+++ etc/applications/helloworld.js
@@ -1,48 +1,47 @@
 {
-	"name": "Hello World",
-	"id": "helloworld",
-	"descr": "A simple Hello World application, running NGINX.",
-	"template": {
-		"rc": {
-			"kind": "ReplicationController",
-			"apiVersion": "v1",
-			"metadata": {
-				"name": "web-server-rc",
-				"labels": {
-					"state": "serving"
-				}
-			},
-			"spec": {
-				"replicas": 1,
-				"selector": {
-					"app": "web-server-v1"
-				},
-				"template": {
-					"metadata": {
-						"labels": {
-							"app": "web-server-v1",
-							"appid": "web-server"
-						}
-					},
-					"spec": {
-						"containers": [{
-							"name": "simpleweb",
-							"image": "nginx",
-							"imagePullPolicy": "Always",
-							"ports": [{
-								"containerPort": 80
-							}]
-						}],
-						"restartPolicy": "Always"
+	"deployment": {
+		"kind": "Deployment",
+		"apiVersion": "extensions/v1beta1",
+		"metadata": {
+		},
+		"spec": {
+			"replicas": 1,
+			"template": {
+				"metadata": {
+					"labels": {
 					}
+				},
+				"spec": {
+					"containers": [{
+						"name": "simpleweb",
+						"image": "nginx",
+						"imagePullPolicy": "Always",
+						"ports": [{
+							"containerPort": 80
+						}]
+					}],
+					"restartPolicy": "Always"
 				}
 			}
 		}
 	},
-	"services": {
-		"sql": {},
-		"dns": {},
-		"dataporten": {},
-		"certificate": {}
-	}
+  "service": {
+    "kind": "Service",
+    "apiVersion": "v1",
+    "metadata": {
+      "name": "hello-world-blah-blah"
+    },
+    "spec": {
+      "selector": {
+        "deployment": "hello-world-blah-blah"
+      },
+      "ports": [{
+        "protocol": "TCP",
+        "port": 80,
+        "targetPort": 80
+      }],
+      "type": "NodePort"
+    }
+  }
+
 }
